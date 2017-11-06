3a8cdb5e789c2f2a30e17f02052970c260be92f5
litfal <litfal1265@gmail.com>
2017-11-06 18:11:24 +0800

    更新版本時將會嘗試使用舊版設定

---
+++ BWHelper.Plugins.Input.AACUSB.Testing/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BWHelper.Plugins.Input.AACUSB.Testing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AACUSBController testController = new AACUSBController();
+            testController.Start(new ConsolePageController());
+            Console.ReadKey();
+        }
+    }
+}
